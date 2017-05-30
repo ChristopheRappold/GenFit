c866be7e51bdd6f874fecefd48bf4ac58fcab4ec
C. Rappold <c.rappold@gsi.de>
2017-05-30 17:16:08 +0200

    Correct compiler warning misleading-indentation
    Correct compiler warning on misleading indentations

--- core/src/MeasurementOnPlane.cc
+++ core/src/MeasurementOnPlane.cc
@@ -53,7 +53,8 @@ void MeasurementOnPlane::Print(Option_t*) const
   printOut << " state vector: "; state_.Print();
   printOut << " covariance matrix: "; cov_.Print();
   if (sharedPlane_ != nullptr)
-    printOut << " defined in plane "; sharedPlane_->Print();
+    printOut << " defined in plane ";
+  sharedPlane_->Print();
   printOut << " hMatrix: "; hMatrix_->Print();
 
 }
